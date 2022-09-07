9dcbd6c2e4f9b45ae8d92c10f96c8127e5652403
Saqlain <iamkhatik786@gmail.com>
2022-09-07 14:45:32 +0530

    Welcome to employee wage computation program using oops

---
+++ EmpWageBuilder.java
@@ -0,0 +1,11 @@
+package com.bl.empwageoops;
+
+public class EmpWageBuilder {
+	
+	public static void main(String[] args) {
+		
+//Main-Branch
+		  System.out.println("Welcome to Employee Wage Computation Program :)");
+	}
+
+}
