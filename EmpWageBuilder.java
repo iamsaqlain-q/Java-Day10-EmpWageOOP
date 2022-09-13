870acf4527f11d245cd17362d7c3d194a225e316
Saqlain <iamkhatik786@gmail.com>
2022-09-13 14:59:59 +0530

    Ability to manage Employee Wage of multiple companies - Note: Refactor

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,39 +5,40 @@ public class EmpWageBuilder {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	
-	private final String companyName; 
-	private final int empRatePerHour; 
-	private final int numOfWorkingDays; 
-	private final int maxHoursInMonth;
-	private int totalEmpWage;
-	
-	public EmpWageBuilder(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
-		this.companyName = companyName;
-		this.empRatePerHour = empRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursInMonth = maxHoursInMonth;
-	}
-	
-	public static void main(String args[]) {
-		EmpWageBuilder google = new EmpWageBuilder("Lenovo", 50, 15, 200);
-		EmpWageBuilder amazon = new EmpWageBuilder("Amazon", 80, 20, 120);
-		EmpWageBuilder netflix = new EmpWageBuilder("Netflix", 90, 18, 220);
+	private int numberOfCompanies = 0;
+	private CompanyEmpWage[] companyWageArray;
 		
-		google.computeEmpWage();
-		System.out.println(google);
-		amazon.computeEmpWage();
-		System.out.println(amazon);
-		netflix.computeEmpWage();
-		System.out.println(netflix);
+	public EmpWageBuilder() {
+		companyWageArray = new CompanyEmpWage[5];
+	}
+
+	public static void main(String args[]) {
+		EmpWageBuilder wageCalculator = new EmpWageBuilder();
+	
+		wageCalculator.addCompany("Google", 50, 15, 200);
+		wageCalculator.addCompany("Amazon", 80, 20, 120);	
+		wageCalculator.addCompany("Netflix", 90, 18, 220);
+		wageCalculator.computeEmpWage();
 	}
 	
-	public void computeEmpWage() {
+	private void addCompany(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
+		companyWageArray[numberOfCompanies] = new CompanyEmpWage(companyName, empRatePerHour, numOfWorkingDays, maxHoursInMonth);
+		numberOfCompanies++;
+	}
+	
+	private void computeEmpWage() {
+		for (int i=0; i<numberOfCompanies; i++) {
+			companyWageArray[i].setTotalEmpWage(this.computeEmpWage(companyWageArray[i]));
+			System.out.println(companyWageArray[i]);
+		}
+	}
+	private int computeEmpWage(CompanyEmpWage companyWage) {
 		int empHrs = 0;
 		int empWage = 0;
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;		
 		
-		for (totalWorkingDays = 1; totalEmpHrs <= maxHoursInMonth && totalWorkingDays < numOfWorkingDays; totalWorkingDays++) {
+		for (totalWorkingDays = 1; totalEmpHrs <= companyWage.maxHoursInMonth && totalWorkingDays < companyWage.numOfWorkingDays; totalWorkingDays++) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			
 			switch (empCheck) {
@@ -55,17 +56,11 @@ public class EmpWageBuilder {
 					empHrs = 0;
 					System.out.println("Employee is absent.");	
 			}			
-			empWage = empHrs * empRatePerHour;
+			empWage = empHrs * companyWage.empRatePerHour;
 			totalEmpHrs += empHrs;
 			//totalEmpWage += empWage;
 			System.out.println("Day " + totalWorkingDays + "	Employee hours : " + empHrs +"	Wage $" + empWage);
 		}		
-		totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println();
-	}
-	
-	@Override
-	public String toString() {
-		return " "+companyName+" company Employee Monthly Wage : $" + totalEmpWage+"\n\n";
+		return (totalEmpHrs * companyWage.empRatePerHour);
 	}
 }
