9fa5f07fa669f4c10e90fefdfb26dcb91c87cdba
Saqlain <iamkhatik786@gmail.com>
2022-09-13 14:50:49 +0530

    total wage for each company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,72 +1,71 @@
 package com.bl.empwageoops;
 
-import java.util.Scanner;
-
 public class EmpWageBuilder {
 	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 	
-	public static void main(String[] args) {
+	private final String companyName; 
+	private final int empRatePerHour; 
+	private final int numOfWorkingDays; 
+	private final int maxHoursInMonth;
+	private int totalEmpWage;
 	
-		computeEmpWage("OnePlus", 50, 15, 200);
-		computeEmpWage("Amazon", 80, 20, 120);
-		computeEmpWage("Meta", 60, 20, 100);
-		computeEmpWage("Oracle", 90, 18, 220);
-		computeEmpWage("Infosys", 100, 20, 250);
+	public EmpWageBuilder(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
+		this.companyName = companyName;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursInMonth = maxHoursInMonth;
 	}
 	
-	public static void computeEmpWage(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
+	public static void main(String args[]) {
+		EmpWageBuilder google = new EmpWageBuilder("Lenovo", 50, 15, 200);
+		EmpWageBuilder amazon = new EmpWageBuilder("Amazon", 80, 20, 120);
+		EmpWageBuilder netflix = new EmpWageBuilder("Netflix", 90, 18, 220);
+		
+		google.computeEmpWage();
+		System.out.println(google);
+		amazon.computeEmpWage();
+		System.out.println(amazon);
+		netflix.computeEmpWage();
+		System.out.println(netflix);
+	}
+	
+	public void computeEmpWage() {
 		int empHrs = 0;
 		int empWage = 0;
-		int totalEmpWage = 0;
 		int totalWorkingDays = 0;
-		int totalEmpHrs = 0;
+		int totalEmpHrs = 0;		
 		
-		System.out.println(" 1.OnePlus \n 2.Amazon \n 3.Meta \n 4.Oracle \n 5.Infosys \n Choose a company : ");
-		Scanner sc = new Scanner(System.in);
-		
-		int choose = sc.nextInt();
-		
-		if(choose <= 5 && choose > 0)
-		{
-		
-		for (totalWorkingDays = 1; totalEmpHrs <= MAX_HRS_IN_MONTH && 
-				totalWorkingDays <= NUM_OF_WORKING_DAYS; totalWorkingDays++) {
-			int empCheck = (int) ((Math.random() * 10) % 3);
+		for (totalWorkingDays = 1; totalEmpHrs <= maxHoursInMonth && totalWorkingDays < numOfWorkingDays; totalWorkingDays++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			
-			switch(empCheck) {
-					
-			  case IS_FULL_TIME :
-				    empHrs = 8;
-					System.out.println("Employee is present : Full Time");
+			switch (empCheck) {
+				case IS_FULL_TIME :
+					empHrs = 8;
+					System.out.println("Employee is present Full time.");
 					break;
 					
-			  case IS_PART_TIME :
+				case IS_PART_TIME :
 					empHrs = 4;
-					System.out.println("Employee is present : Part Time");
+					System.out.println("Employee is present Part time.");
 					break;
 					
 				default :
 					empHrs = 0;
-					System.out.println("Employee is absent");	
-			}
-			
-			empWage = empHrs * EMP_RATE_PER_HOUR;
+					System.out.println("Employee is absent.");	
+			}			
+			empWage = empHrs * empRatePerHour;
 			totalEmpHrs += empHrs;
-			totalEmpWage += empWage;
-			System.out.println("Day " + totalWorkingDays + " Employee hours : " + empHrs +"	Wage $" + empWage);
-		}
-		
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("\n Employee Monthly Wage : $" + totalEmpWage);
+			//totalEmpWage += empWage;
+			System.out.println("Day " + totalWorkingDays + "	Employee hours : " + empHrs +"	Wage $" + empWage);
+		}		
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println();
 	}
-		else
-		{
-			System.out.println("Invalid Input ");	
-		}
+	
+	@Override
+	public String toString() {
+		return " "+companyName+" company Employee Monthly Wage : $" + totalEmpWage+"\n\n";
 	}
 }
